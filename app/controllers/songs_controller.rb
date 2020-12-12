3ad91f18d8475a9bcd79ea59b4129d56051746de
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-12 08:20:23 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,7 +19,7 @@ class SongsController < ApplicationController
 
   def update
     @song = Song.find(params[:id])
-    @song.update(song_params(:name))
+    @song.update(song_params(:name, :artist_id, :genre_id)))
     redirect_to song_path(@song)
   end
 
